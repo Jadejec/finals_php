ad4f917867bf100bdd8a7caf3781f09825707601
Jade Cordero <jadejec.jc@gmail.com>
2024-11-21 23:57:20 +0800

    Logout features

--- admin/logout.php
+++ admin/logout.php
@@ -1,3 +1,18 @@
 <?php
     // Logout Code Here
+ 
+// Start the session
+session_start();
+
+// Clear all session variables
+session_unset();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to the login page
+header("Location: ../index.php");
+exit;
+
+
 ?>
